186a23c87fd7a27b008ae7434c4660d36ccffbdb
jax <jax@local>
2026-04-17 20:53:44 +0000

    update

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-welcome = input("hello, world!, what is your name? ")
-print(f"nice to meet you,{welcome}")
-
-message2 = input("would you like to play a game? ")
-if message2 == "yes":
-    print("ok!, lets play a game ")
+welcome = input("Hello! What is your name? ")
+print(f"Nice to meet you, {welcome}")
+ 
+message2 = input("Would you like to play a game? (yes/no) ")
+ 
+if message2.lower() == "yes":
+     print("Ok! Let's play a game 🎮")
 else:
- print("booo!!boooo!")
+     print("booo!! 😄")
