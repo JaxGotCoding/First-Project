53dd94ad877e57d97ce0af5c599e94ca0b436cd9
jax <jax@local>
2026-04-17 20:39:59 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+welcome = input("hello, world!, what is your name? ")
+print(f"nice to meet you,{welcome}")
+
+message2 = input("would you like to play a game? ")
+if message2 == "yes":
+    print("ok!, lets play ")
+else:
+ print("booo!!")
